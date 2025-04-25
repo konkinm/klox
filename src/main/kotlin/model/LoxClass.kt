d42cef5f0baefbbb8dc06a2804f3cb5a1a61ee21
MaxKonkin <knkn.max@yandex.ru>
2025-04-25 19:36:40 +0300

    Classes: Creating Instances

--- src/main/kotlin/model/LoxClass.kt
+++ src/main/kotlin/model/LoxClass.kt
@@ -1,8 +1,19 @@
 package model
 
-class LoxClass(val name: String) {
+import Interpreter
+
+class LoxClass(val name: String): LoxCallable {
 
     override fun toString(): String {
         return name
+    }
+
+    override fun arity(): Int {
+        return 0
+    }
+
+    override fun call(interpreter: Interpreter, arguments: List<Any?>): Any? {
+        val instance = LoxInstance(this)
+        return instance
     }
 }
