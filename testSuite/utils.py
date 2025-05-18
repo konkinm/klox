76db5bc909acb465f2c1f7855b1cf81485ca0edd
MaxKonkin <knkn.max@yandex.ru>
2025-05-18 13:53:29 +0300

    build-and-test.yaml action introduced

--- testSuite/utils.py
+++ testSuite/utils.py
@@ -10,7 +10,7 @@ class LoxCommandResult(NamedTuple):
 
 
 def run_lox(lox, file):
-    cmd = f"{lox} {file}"
+    cmd = lox.split() + [file]
     process = subprocess.run(
         cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
@@ -20,9 +20,3 @@ def run_lox(lox, file):
     return LoxCommandResult(
         stdout=out, stderr=err, exit_code=process.returncode
     )
-
-def main():
-    print(run_lox("java -jar C:/Users/Max/AppData/Local/Temp/codecrafters-build-dir/build-your-own-interpreter.jar run", "lox/function/nested_call_with_arguments.lox"))
-    
-if __name__ == '__main__':
-    main()
